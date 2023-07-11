0cee4c3d4c9b26586be998a76985d5d2d3e2b35f
vagner0795 <vagner-se@hotmail.com>
2023-07-11 11:00:12 -0300

    feat: created eslint and prettier config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
+import React from 'react'
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>DevSteam: A sua loja online de games</title>
-        <meta name="description" content="DevSteam: A sua loja online de games" />
+        <title>DevShopGames: A sua loja online de games</title>
+        <meta name="description" content="DevShopGames: A sua loja online de games" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        DevStart: A sua loja online de games
-      </div>
+      <div>DevStart: A sua loja online de games</div>
     </>
   )
 }
